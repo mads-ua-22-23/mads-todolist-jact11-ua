801c12100766d95a3ee9eca6b6f3d059662b032a
Jose Antonio Cubi <jact11@gcloud.ua.es>
2022-11-05 15:34:16 +0100

    Solucionado un error en el test de listado de equipos

--- src/test/java/madstodolist/EquipoWebTest.java
+++ src/test/java/madstodolist/EquipoWebTest.java
@@ -1,70 +1,58 @@
 package madstodolist;
 
-import madstodolist.authentication.ManagerUserSession;
 import madstodolist.model.Equipo;
 import madstodolist.model.EquipoRepository;
 import madstodolist.model.Usuario;
-import madstodolist.model.UsuarioRepository;
 import madstodolist.service.EquipoService;
 import madstodolist.service.UsuarioService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.Conversions.string;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
+@Sql(scripts = "/clean-db.sql", executionPhase = AFTER_TEST_METHOD)
 @AutoConfigureMockMvc
-@Sql(scripts="/clean-db.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-//
-// A diferencia de los tests web de tarea, donde usábamos los datos
-// de prueba de la base de datos, aquí vamos a practicar otro enfoque:
-// moquear el usuarioService.
 public class EquipoWebTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    @MockBean
-    private ManagerUserSession managerUserSession;
+    private EquipoRepository equipoRepository;
 
     @Autowired
-    UsuarioRepository usuarioRepository;
-    @Autowired
-    EquipoRepository equipoRepository;
-    @MockBean
     private UsuarioService usuarioService;
-
-    @MockBean
+    @Autowired
     private EquipoService equipoService;
 
     @Test
-    public void ListadoUsuariosTest() throws Exception{
+    public void ListadoEquiposTest() throws Exception{
         Usuario user = new Usuario("prueba@ua");
         user.setPassword("123");
         usuarioService.registrar(user);
 
-        Equipo equipo=new Equipo("equipo");
+        Equipo equipo=equipoService.crearEquipo("equipoPrueba");
         equipoRepository.save(equipo);
+        Equipo equipo2=equipoService.crearEquipo("pruebaEquipo");
+        equipoRepository.save(equipo2);
 
         this.mockMvc.perform(get("/equipos"))
                 .andExpect((content().string(allOf(
                         containsString("Nombre"),
-                        containsString(equipo.getNombre())
+                        containsString(equipo.getNombre()),
+                        containsString("1"),
+                        containsString(equipo2.getNombre()),
+                        containsString("2")
                 ))));
     }
 }
